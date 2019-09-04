b8a6cec940da4d02c130df5e63c3794531de7942
William Perkins <william.perkins@pnnl.gov>
2019-09-04 08:55:12 -0700

    Add a script to check results against serial output

---
+++ RainyCreek/check_serial.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -ue
+
+# -------------------------------------------------------------
+# initialize variables
+# -------------------------------------------------------------
+
+# Point this at the local numdiff
+numdiff=numdiff
+
+# Directory with expected output files
+gooddir="../output.serial"
+
+# Relative numeric error to except (this is really low)
+relerr="1.0E-04"
+
+# Output files to check
+files=" \
+    Aggregated.Values \
+    ATP.Only \
+    Inflow.Only \
+    Mass.Balance \
+    Mass.Final.Balance \
+    Melt.Only \
+    NLW.Only \
+    NSW.Only \
+    Outflow.Only \
+    Stream.Flow \
+    Streamflow.Only \
+    VP.Only \
+    WND.Only \
+"
+
+# -------------------------------------------------------------
+# main program
+# -------------------------------------------------------------
+
+
+for i in $files; do
+    if [ -f "$i" ]; then
+        echo $i
+        numdiff -r "$relerr" $i "${gooddir}/$i" || true
+    else
+        echo "$i not found"
+    fi
+done
